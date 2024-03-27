832f4f687d5e11fac071e516f0bfac914d6bc7de
linzy5-fuyao <linzy5@xiaopeng.com>
2024-03-27 11:45:55 +0000

    [zeal] fix bug to compile with transformers==4.39
    Signed-off-by: linzy5-fuyao <linzy5@xiaopeng.com>

--- llava/model/builder.py
+++ llava/model/builder.py
@@ -30,9 +30,15 @@ def load_pretrained_model(model_path, model_base, model_name, load_8bit=False, l
         kwargs['device_map'] = {"": device}
 
     if load_8bit:
-        kwargs['load_in_8bit'] = True
+        # kwargs['load_in_8bit'] = True
+        kwargs['quantization_config'] = BitsAndBytesConfig(
+            load_in_8bit=True,
+            bnb_8bit_compute_dtype=torch.float16,
+            bnb_8bit_use_double_quant=True,
+            bnb_8bit_quant_type='nf8'
+        )
     elif load_4bit:
-        kwargs['load_in_4bit'] = True
+        # kwargs['load_in_4bit'] = True
         kwargs['quantization_config'] = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_compute_dtype=torch.float16,
